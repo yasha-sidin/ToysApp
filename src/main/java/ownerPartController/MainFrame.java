4f8341f17957d58f8a0a133df9597143c44801cf
Yasha Sidin <124708836+yasha-sidin@users.noreply.github.com>
2023-08-18 03:01:05 +0300

    Set visual part of owner main frame

--- src/main/java/ownerPartController/MainFrame.java
+++ src/main/java/ownerPartController/MainFrame.java
@@ -8,24 +8,27 @@ import toysMachineApi.ToysMachineApi;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.swing.*;
 import javax.swing.border.EmptyBorder;
+import javax.swing.plaf.basic.BasicSplitPaneUI;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.ArrayList;
 import java.util.ConcurrentModificationException;
 import java.util.HashMap;
 import java.util.List;
 
 public class MainFrame extends JFrame {
     private final Font textAreaFont = new Font("Times New Roman", Font.PLAIN, 14);
-    private final Font labelFont = new Font("Times New Roman", Font.PLAIN, 20);
-    private final Font buttonFont = new Font("Times New Roman", Font.PLAIN, 20);
-    private TextArea allToys;
+    private final Font labelFont = new Font("Times New Roman", Font.PLAIN, 18);
+    private final Font buttonFont = new Font("Times New Roman", Font.PLAIN, 18);
+    private JLabel paramsLabel;
+    private JTextArea allToys;
     private JButton addButton;
     private JButton settingsButton;
     private JButton updateProbabilityButton;
     private JButton deleteButton;
     private static HashMap<Integer, Toy> toysMap;
-    private JScrollBar scrollBar;
+    private JScrollPane scrollPane;
     private final ToysMachineApi toysMachineApi;
 
     public MainFrame(iGetModel<Toy> model) {
@@ -34,32 +37,72 @@ public class MainFrame extends JFrame {
     }
     private void initialize() {
         setTitle("ToysMachineOwnerApp");
-        setSize(900, 800);
-        setMaximumSize(new Dimension(300, 400));
+        setResizable(false);
+        setMinimumSize(new Dimension(1000, 900));
+        setMaximumSize(new Dimension(1000, 900));
         Toolkit toolkit = getToolkit();
         Dimension size = toolkit.getScreenSize();
         setLocation(size.width/2 - getWidth()/2, size.height/2 - getHeight()/2);
+        setLayout(new BorderLayout(50, 50));
 
         JPanel externalPanel = new JPanel(new BorderLayout());
         externalPanel.setBorder(new EmptyBorder(new Insets(15, 15, 15 , 15)));
 
         JPanel internalPanel = new JPanel();
-        internalPanel.setBackground(Color.gray);
+        internalPanel.setBackground(new Color(229, 216, 216));
         internalPanel.setPreferredSize(new Dimension(250, 150));
 
-        /*
+        // SETTINGS PANEL
+        JPanel settingsPanel = new JPanel();
+        settingsPanel.setBackground(new Color(229, 216, 216));
+        paramsLabel = new JLabel();
+        paramsLabel.setFont(labelFont);
+        paramsLabel.setText(getSensitiveData());
+        settingsPanel.add(paramsLabel);
 
-          CREATING INFO PART OF THE FRAME
-
-         */
+        // ALL TOYS
         JPanel infoPanel = new JPanel();
         infoPanel.setBackground(new Color(229, 216, 216));
-        JLabel paramsLabel = new JLabel();
-        paramsLabel.setText(getSensitiveData());
-        infoPanel.add(paramsLabel);
+        allToys = new JTextArea();
+        allToys.setFont(textAreaFont);
+        allToys.setEditable(true);
+        allToys.setText(getToysList());
+        allToys.setRows(40);
+        allToys.setColumns(65);
+        allToys.setBackground(new Color(240, 243, 252));
+        scrollPane = new JScrollPane(allToys, JScrollPane.VERTICAL_SCROLLBAR_ALWAYS, JScrollPane.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        infoPanel.add(scrollPane);
+
+        // BUTTON PANEL
+        JPanel buttonsPanel = new JPanel();
+        buttonsPanel.setBackground(new Color(229, 216, 216));
+        Box box = new Box(BoxLayout.X_AXIS);
+        addButton = new JButton("Add");
+        addButton.setFont(buttonFont);
+        settingsButton = new JButton("Settings");
+        settingsButton.setFont(buttonFont);
+        updateProbabilityButton = new JButton("Update Toy");
+        updateProbabilityButton.setFont(buttonFont);
+        deleteButton = new JButton("Delete");
+        deleteButton.setFont(buttonFont);
+        box.add(addButton);
+        box.add(Box.createRigidArea(new Dimension(60, 0)));
+        box.add(settingsButton);
+        box.add(Box.createRigidArea(new Dimension(60, 0)));
+        box.add(updateProbabilityButton);
+        box.add(Box.createRigidArea(new Dimension(60, 0)));
+        box.add(deleteButton);
+        buttonsPanel.add(box);
 
 
-        internalPanel.add(infoPanel);
+
+
+
+        // ASSEMBLING COMPONENTS
+        internalPanel.add(settingsPanel, BorderLayout.NORTH);
+        internalPanel.add(infoPanel, BorderLayout.CENTER);
+        internalPanel.add(buttonsPanel, BorderLayout.SOUTH);
+
         externalPanel.add(internalPanel);
         add(externalPanel);
 
@@ -68,7 +111,9 @@ public class MainFrame extends JFrame {
     }
 
     private void fillToysMap() {
-        toysMap.clear();
+        if (toysMap != null) {
+            toysMap.clear();
+        }
         List<Toy> toysList = toysMachineApi.getAllToys();
         for (int i = 1; i < toysList.size(); i++) {
             toysMap.put(i, toysList.get(i - 1));
@@ -77,6 +122,9 @@ public class MainFrame extends JFrame {
 
     private String getToysList() {
         fillToysMap();
+        if (toysMap == null) {
+            return "";
+        }
         StringBuilder stringBuilder = new StringBuilder();
         for (int key : toysMap.keySet()) {
             stringBuilder.append(key).append(". ").append(toysMap.get(key).printToOwner()).append("\n");
@@ -88,7 +136,7 @@ public class MainFrame extends JFrame {
         StringBuilder stringBuilder = new StringBuilder();
         stringBuilder.append("Current probability: ")
                      .append(toysMachineApi.getCurrentProbability().getValue())
-                     .append(" ")
+                     .append(";  ")
                      .append("Settings values: { minAmountOfToys: ")
                      .append(toysMachineApi.getMinAmountOfToys())
                      .append(", choice's cost: ")
